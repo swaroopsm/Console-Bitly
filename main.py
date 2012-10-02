4ddd29b9355c0e8f19bcfaa01da01baa55363b38
Swaroop SM <swaroop.striker@gmail.com>
2012-10-02 19:42:02 +0530

    display value of url in json obj with a cyan colored text

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import settings as s
 
 req=raw_input("Enter the URL to be shortened: ")
 url="http://api.bitly.com/v3/shorten?login="+s.bitly_username+"&apiKey="+s.bitly_apikey+"&longUrl="+req
+print"\Please wait... \n"
 response=urllib2.urlopen(url)
 a = json.loads(response.read())
-print a['data']['hash']
+print "\nShortened URL is: \033[1;36m"+a['data']['url']+"\033[1;m\n"
