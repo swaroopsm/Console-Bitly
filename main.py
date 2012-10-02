06a7f2333f360b5d37ad4c686becda4b40e7d1cf
Swaroop SM <swaroop.striker@gmail.com>
2012-10-02 19:59:53 +0530

    wrapped in a class and defined shorten method

--- main.py
+++ main.py
@@ -4,9 +4,16 @@ import urllib2
 import json
 import settings as s
 
+class ConsoleBitly:
+	def shorten(self,req):
+		url="http://api.bitly.com/v3/shorten?login="+s.bitly_username+"&apiKey="+s.bitly_apikey+"&longUrl="+req
+		print "Please wait... \n"
+		response=urllib2.urlopen(url)
+		a = json.loads(response.read())
+		print "\nShortened URL is: \033[1;36m"+a['data']['url']+"\033[1;m\n"
+
+
+c=ConsoleBitly()
 req=raw_input("Enter the URL to be shortened: ")
-url="http://api.bitly.com/v3/shorten?login="+s.bitly_username+"&apiKey="+s.bitly_apikey+"&longUrl="+req
-print"\Please wait... \n"
-response=urllib2.urlopen(url)
-a = json.loads(response.read())
-print "\nShortened URL is: \033[1;36m"+a['data']['url']+"\033[1;m\n"
+c.shorten(req)
+
