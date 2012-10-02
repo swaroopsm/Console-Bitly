f80d9b670d0e57a756e6c8e4ec5c73d8f2dce3d6
Swaroop SM <swaroop.striker@gmail.com>
2012-10-02 19:07:23 +0530

    shorten url and display the hash value

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import urllib2
 import json
 import settings as s
 
-url=raw_input("Enter the URL to be shortened: ")
-
-print s.bitly_username
+req=raw_input("Enter the URL to be shortened: ")
+url="http://api.bitly.com/v3/shorten?login="+s.bitly_username+"&apiKey="+s.bitly_apikey+"&longUrl="+req
+response=urllib2.urlopen(url)
+a = json.loads(response.read())
+print a['data']['hash']
