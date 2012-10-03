b95ab010c61df3a0be33f9ebc528fe0b8770d4e7
Swaroop SM <swaroop.striker@gmail.com>
2012-10-04 01:10:41 +0530

    created setup file with basic requirements

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(
+    name='ConsoleBitly',
+    version='0.1',
+    url="http://github.com/swaroopsm/Console-Bitly",
+    license='MIT Licence',
+    author="Swaroop SM",
+    author_email="swaroop.striker@gmail.com",
+    description="A thin wrapper around the bit.ly API",
+    packages=['ConsoleBitly']
+    
+)
