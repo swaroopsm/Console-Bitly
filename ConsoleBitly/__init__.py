397bbd224f64f73235ac05cf79506a4e25ae27ce
Swaroop SM <swaroop.striker@gmail.com>
2012-10-06 09:46:53 +0530

    replaced static methods to normal class methods and used constructor to initialize class variables

--- ConsoleBitly/__init__.py
+++ ConsoleBitly/__init__.py
@@ -2,13 +2,15 @@
 import urllib
 import urllib2
 import json
-import settings as s
 
 class ConsoleBitly:
-	@staticmethod
-	def shorten(req,self=None):
+	def __init__(self,bitly_username,bitly_apikey):
+		self.bitly_username=bitly_username
+		self.bitly_apikey=bitly_apikey
+		
+	def shorten(self,req):
 		try:
-			url="http://api.bitly.com/v3/shorten?login="+s.bitly_username+"&apiKey="+s.bitly_apikey+"&longUrl="+req
+			url="http://api.bitly.com/v3/shorten?login="+self.bitly_username+"&apiKey="+self.bitly_apikey+"&longUrl="+req
 			print "Please wait... \n"
 			response=urllib2.urlopen(url)
 			a = json.loads(response.read())
@@ -16,10 +18,9 @@ class ConsoleBitly:
 		except:
 			print "\033[1;31mThe provided url may be invalid. Prefix URL with http OR https...\033[1;m\n"
 	
-	@staticmethod	
-	def expand(req,self=None):
+	def expand(self,req):
 		try:
-			url="http://api.bitly.com/v3/expand?login="+s.bitly_username+"&apiKey="+s.bitly_apikey+"&shortUrl="+req
+			url="http://api.bitly.com/v3/expand?login="+self.bitly_username+"&apiKey="+self.bitly_apikey+"&shortUrl="+req
 			print "Please wait... \n"
 			response=urllib2.urlopen(url)
 			a = json.loads(response.read())
@@ -27,10 +28,9 @@ class ConsoleBitly:
 		except:
 			print "\033[1;31mThe provided url might not be of a bit.ly domain OR shortened version does not exist!\033[1;m\n"
 	
-	@staticmethod
-	def info(req,self=None):
+	def info(self,req):
 		try:
-			url="http://api.bitly.com/v3/info?login="+s.bitly_username+"&apiKey="+s.bitly_apikey+"&shortUrl="+req
+			url="http://api.bitly.com/v3/info?login="+self.bitly_username+"&apiKey="+self.bitly_apikey+"&shortUrl="+req
 			print "Please wait... \n"
 			response=urllib2.urlopen(url)
 			a = json.loads(response.read())
