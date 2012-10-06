3cd4ca042e2906041a1f90fef82cad9103f17cd0
Swaroop SM <swaroop.striker@gmail.com>
2012-10-06 09:47:30 +0530

    removed default.settings file

--- ConsoleBitly/default.settings.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-##                                   ##
-##  Rename this file to settings.py  ##
-##																	 ##
-
-bitly_username="YOUR_BIT.LY_USERNAME"
-bitly_apikey="YOUR_BIT.LY_API_KEY"
